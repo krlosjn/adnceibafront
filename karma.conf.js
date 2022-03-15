80b4211c87a84406a8b5ca87d2a1c0ec1f89a4e4
carlos.junco <carlos.junco@ceiba.com.co>
2022-03-15 14:29:38 -0500

    modifica package.json y karma.confif

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config) {
         logLevel: config.LOG_INFO,
         autoWatch: true,
         browsers: ['ChromeHeadless'],
-        singleRun: false,
+        singleRun: true,
         restartOnFileChange: true
     });
 };
